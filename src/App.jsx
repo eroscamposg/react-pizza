2b09921574ae91836e560840a4c6b0f7034d890b
Eros Campos <eroscamposg@gmail.com>
2026-03-30 18:46:17 -0500

    Submitting forms with react router actions

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
@@ -30,7 +30,8 @@ let router = createBrowserRouter([
       },
       {
         path: "/order/new",
-        Component: CreateOrder
+        Component: CreateOrder,
+        action: createOrderAction
       },
       {
         path: "/order/:orderId",
