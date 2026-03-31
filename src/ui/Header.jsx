77cbdefffa1d4da6412829ed4cf19084f7020d67
Eros Campos <eroscamposg@gmail.com>
2026-03-31 18:11:07 -0500

    Adding prettier and tailwind config

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
-    return (
-        <header>
-            <Link to='/'>Fast React Pizza Co.</Link>
-            <SearchOrder />
-            <p>Eros</p>
-      </header>
-  )
+  return (
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
+      <p>Eros</p>
+    </header>
+  );
 }
